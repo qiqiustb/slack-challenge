5f5f80473664bd0e0f329e6711b2768310b0bb46
qiqiustb <qiqjob89@gmail.com>
2016-09-25 22:30:43 +0000

    Fix search bar

--- app/view_header/header.js
+++ app/view_header/header.js
@@ -1,11 +1,18 @@
 (function(){
   'use strict';
   angular
-    .module('myApp.header', [])
-    .controller('HeaderController', HeaderController);
+    .module('slackChallenge.header', [])
+    .controller('HeaderController', ['$scope', '$location', HeaderController]);
 
-  function HeaderController ($scope){
-    $scope.myheader = 'hello world';
+  function HeaderController ($scope, $location){
+
+    $scope.search = function() {
+      // fetch HTML source
+      var url = $scope.searchterm;
+      //$state.go('app.view1', {run_id: matches[0]});
+      // if string doesn't match, then its probably user/branch/sha
+      $location.path('/view1').search({'url': $scope.searchterm});
+    };
   };
 
 })();
