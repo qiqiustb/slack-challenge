704b775d56ed8450c94c0940c58777ae5903a7c2
qiqiustb <qiqjob89@gmail.com>
2016-09-26 03:25:21 +0000

    Broadcast error message

--- angular-app/app/view1/view1.js
+++ angular-app/app/view1/view1.js
@@ -8,15 +8,16 @@
   function View1Ctrl ($scope, $stateParams, $http) {
     $scope.url = $stateParams.url;
     if(angular.isDefined($scope.url)){
-    $http({
-      url: '/api/url?url=' + $scope.url,
-      method: 'GET',
-    }).success(function(data){
-      $scope.data = data;
-      $scope.$broadcast('data', $scope.data);
-    }).error(function(error){
-      console.log('error');
-    });
+      $http({
+        url: '/api/url?url=' + $scope.url,
+        method: 'GET',
+      }).success(function(data){
+        $scope.data = data;
+        $scope.$broadcast('data', $scope.data);
+      }).error(function(error){
+        console.log(error);
+        $scope.$broadcast('data', {'summary': {}, 'source': 'Error while fetching ' + $scope.url + '.\n\n' + error});
+      });
     }
   };
 })();
